d91f47a25770337b8094c1091453924dac5177fb
OrangeDev2 <sabym777@gmail.com>
2021-01-01 21:47:12 -0800

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, resizeMode, ImageBackground } from 'react-native';
 
-export default function App() {
+class App extends Component {
+  render(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_9e_4xzxk--nNCIqJxTpMZ_Nx1_obutxKSg&usqp=CAU'}} style={styles.backgroundStyle}>
+      <View style={styles.someContainer}>
+        <Text style={styles.textStyle}>Hello World!</Text>
+      </View>
+    </ImageBackground>
   );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  someContainer: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: 'white',
+  },
+  backgroundStyle: {
+    flex: 1,
+    height: null,
+    width: null,
+    resizeMode: 'cover',
+  },
 });
+
+export default App;
